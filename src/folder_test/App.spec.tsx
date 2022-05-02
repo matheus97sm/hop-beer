f1115ac44ab73dae6ac2e4164b4ebfcb97ced403
matheus97sm <matheus97sm@gmail.com>
2022-05-02 14:25:27 -0300

    Changed the file

--- src/folder_test/App.spec.tsx
+++ src/folder_test/App.spec.tsx
@@ -1,8 +1,4 @@
 import { render } from '@testing-library/react';
 import { App } from './App';
 
-test('sum', () => {
-  const { getByText } = render(<App />);
-
-  expect(getByText('Developed by:')).toBeInTheDocument();
-});
+test('sum', () => {});
